ef0199764a353dcce22649f44654a3a60de462a8
jeff <step4u@gmail.com>
2024-10-23 17:42:16 +0900

    test 추가

---
+++ Test/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class MainWindowViewModel: ObservableObject
+    {
+        public ISeries[] Series { get; set; } =
+    {
+        new LineSeries<double>
+        {
+            Values = new double[] { 0, 3, 5, 3, 4, 6 },
+            Fill = new SolidColorPaint(SKColors.CornflowerBlue),
+            Stroke = null,
+            GeometryFill = null,
+            GeometryStroke = null
+        }
+    };
+
+        // Creates a gray background and border in the draw margin.
+        public DrawMarginFrame DrawMarginFrame => new()
+        {
+            Fill = new SolidColorPaint(new SKColor(220, 220, 220)),
+            Stroke = new SolidColorPaint(new SKColor(180, 180, 180), 1)
+        };
+    }
+}
